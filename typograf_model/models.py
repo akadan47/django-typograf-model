00569c4cc1d261cb51f7c642e916b90882fad501
Denis Popov <akadan47@gmail.com>
2014-05-08 10:42:25 +0400

    Fix fields

--- typograf_model/models.py
+++ typograf_model/models.py
@@ -7,10 +7,10 @@ from .typograf import typo
 class TypoModel(models.Model):
 
     def save(self, *args, **kwargs):
-        for field_name in self._typo_fields:
+        for field_name in getattr(self, "_typo_fields", []):
             value = typo(getattr(self, field_name), is_para=False)
             setattr(self, field_name, smart_unicode(value))
-        for field_name in self._typo_fields_para:
+        for field_name in getattr(self, "_typo_fields_para", []):
             value = typo(getattr(self, field_name), is_para=False)
             setattr(self, field_name, smart_unicode(value))
         super(TypoModel, self).save(*args, **kwargs)
